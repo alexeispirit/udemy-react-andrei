90b139331f629a887ec2d7e956f52905b0a6d296
alexeispirit <alexeispirit@gmail.com>
2019-08-01 17:23:49 +0500

    120-CheckoutItem-Component

--- crwn-clothing/src/pages/checkout/checkout.js
+++ crwn-clothing/src/pages/checkout/checkout.js
@@ -2,12 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import CheckoutItem from "../../components/checkout-item/checkout-item";
+
 import {
   selectCartItems,
   selectCartTotal
 } from "../../redux/cart/cart-selectors";
 
 import "./checkout.scss";
+import CartItem from "../../components/cart-item/cart-item";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -28,7 +31,9 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => cartItem.name)}
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
