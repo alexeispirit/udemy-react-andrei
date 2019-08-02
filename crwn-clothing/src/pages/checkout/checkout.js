636c0c157c74ced744fea1091d7c78ccd5ab2fb7
alexeispirit <alexeispirit@gmail.com>
2019-08-02 11:52:13 +0500

    137-Stripe-frontend-integration

--- crwn-clothing/src/pages/checkout/checkout.js
+++ crwn-clothing/src/pages/checkout/checkout.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button";
 
 import {
   selectCartItems,
@@ -10,7 +11,6 @@ import {
 } from "../../redux/cart/cart-selectors";
 
 import "./checkout.scss";
-import CartItem from "../../components/cart-item/cart-item";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -34,9 +34,13 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 Exp: 02/22 CVC: 222
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
