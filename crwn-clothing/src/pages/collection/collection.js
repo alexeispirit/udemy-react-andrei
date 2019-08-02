81d8072aea8dc23288bd63188c470051b834f977
alexeispirit <alexeispirit@gmail.com>
2019-08-02 10:20:47 +0500

    129-Improving-naming-of-component

---
+++ crwn-clothing/src/pages/collection/collection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item";
+
+import "./collection.scss";
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>CollectionPage</h2>
+  </div>
+);
+
+export default CollectionPage;
