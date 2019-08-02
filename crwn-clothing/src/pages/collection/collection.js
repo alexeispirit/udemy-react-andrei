a6ca260a1d6af77114347fc805dd538428279425
alexeispirit <alexeispirit@gmail.com>
2019-08-02 10:27:17 +0500

    130-Collection-routing-and-selector

--- crwn-clothing/src/pages/collection/collection.js
+++ crwn-clothing/src/pages/collection/collection.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCollection } from "../../redux/shop/shop-selectors";
 
 import CollectionItem from "../../components/collection-item/collection-item";
 
 import "./collection.scss";
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
   <div className="collection-page">
     <h2>CollectionPage</h2>
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
