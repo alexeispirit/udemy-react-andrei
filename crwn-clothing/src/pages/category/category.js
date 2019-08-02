e03e8f0cb84ec582b48c981cb63dcfe6f20ad53b
alexeispirit <alexeispirit@gmail.com>
2019-08-02 10:17:43 +0500

    128-Nested-routing-in-ShopPage

---
+++ crwn-clothing/src/pages/category/category.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item";
+
+import "./category.scss";
+
+const CategoryPage = ({ match }) => (
+  <div className="category">
+    <h2>CategoryPage</h2>
+  </div>
+);
+
+export default CategoryPage;
