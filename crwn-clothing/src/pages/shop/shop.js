7e558df4b918467411a55d91372f4eaa7435fe4d
alexeispirit <alexeispirit@gmail.com>
2019-08-05 17:37:42 +0500

    176-Redux-thunk-into-saga

--- crwn-clothing/src/pages/shop/shop.js
+++ crwn-clothing/src/pages/shop/shop.js
@@ -1,26 +1,16 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import CollectionsOverview from "../../components/collections-overview/collections-overview";
-import CollectionPage from "../collection/collection";
-
-import WithSpinner from "../../components/with-spinner/with-spinner";
-
-import { fetchCollectionsStartAsync } from "../../redux/shop/shop-actions";
-import {
-  selectIsCollectionFetching,
-  selectIsCollectionsLoaded
-} from "../../redux/shop/shop-selectors";
+import { fetchCollectionsStart } from "../../redux/shop/shop-actions";
 
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview-container";
 import CollectionPageContainer from "../collection/collection-container";
 
 class ShopPage extends Component {
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props;
-    fetchCollectionsStartAsync();
+    const { fetchCollectionsStart } = this.props;
+    fetchCollectionsStart();
   }
 
   render() {
@@ -42,7 +32,7 @@ class ShopPage extends Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(
