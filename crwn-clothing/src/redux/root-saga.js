e778275227fba810475ce014084149ceb344c629
alexeispirit <alexeispirit@gmail.com>
2019-08-06 11:11:33 +0500

    180-Google-sign-in-into-sagas

--- crwn-clothing/src/redux/root-saga.js
+++ crwn-clothing/src/redux/root-saga.js
@@ -1,7 +1,8 @@
 import { all, call } from "redux-saga/effects";
 
+import { userSagas } from "./user/user-sagas";
 import { fetchCollectionStart } from "./shop/shop-sagas";
 
 export default function* rootSaga() {
-  yield all([call(fetchCollectionStart)]);
+  yield all([call(fetchCollectionStart), call(userSagas)]);
 }
