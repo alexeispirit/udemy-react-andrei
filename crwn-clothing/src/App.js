fd98c3b97677cf0fc1e05dfe40e738bf21045eb9
alexeispirit <alexeispirit@gmail.com>
2019-08-09 11:21:11 +0500

    239-Error-boundaries

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -9,6 +9,7 @@ import { createStructuredSelector } from "reselect";
 // import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import Header from "./components/header/header";
 import Spinner from "./components/spinner/spinner";
+import ErrorBoundary from "./components/error-boundary/error-boundary";
 
 import { GlobalStyle } from "./global.styles";
 
@@ -32,18 +33,20 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Suspense fallback={<Spinner />}>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            exact
-            path="/signin"
-            render={() =>
-              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
-            }
-          />
-        </Suspense>
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage} />
+            <Route
+              exact
+              path="/signin"
+              render={() =>
+                currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+              }
+            />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
